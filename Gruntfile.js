79f3ef82d47161c3f67f7ec70553371714999fcf
Cam <cam@mackay.net.nz>
2015-02-14 11:02:31 +1300

    added grunt plugins

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.export = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				banner: '/*! <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+			},
+			build: {
+				src: 'src/<%= pkg.name %>.js',
+				dest: 'build/<%= pkg.name %>.min.js'
+			}
+		}
+	});
+
+	// Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	// Default tasks(s).
+	grunt.registerTask('default', ['uglify']);
+
+}
